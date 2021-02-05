d05015b6f11186bf9874881ad31681fac08681d1
Alex Fontan <afontanmoreira@danielcastelao.org>
2021-02-05 09:21:26 +0100

    Commit C de la rama lider

--- src/com/company/Alex.java
+++ src/com/company/Alex.java
@@ -4,5 +4,6 @@ public class Alex {
     public Alex() {
         System.out.println("Commit A de la rama lider");
         System.out.println("Commit B de la rama lider");
+        System.out.println("Commit C de la rama lider");
     }
 }
