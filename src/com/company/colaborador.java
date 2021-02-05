7865d65339c306a35fe970a676cf025055e3c675
Alex Fontan <afontanmoreira@danielcastelao.org>
2021-02-05 09:49:28 +0100

    Funcionalidad A lista

---
+++ src/com/company/colaborador.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class colaborador {
+
+    public void primerCambio(){
+
+        System.out.println(" primer cambio ");
+    }
+    public void segundoCambio(){
+        System.out.println("segundo cambio");
+    }
+    public void tercerCambio(){
+        System.out.println("tercer cambio");
+    }
+}
