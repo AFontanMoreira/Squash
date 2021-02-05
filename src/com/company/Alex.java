f649fc69eed1854e3edea6102b058c311066ecc2
Alex Fontan <afontanmoreira@danielcastelao.org>
2021-02-05 09:50:23 +0100

    Funcionalidad B lista

---
+++ src/com/company/Alex.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Alex {
+    public Alex() {
+        System.out.println("Commit A de la rama lider");
+        System.out.println("Commit B de la rama lider");
+        System.out.println("Commit C de la rama lider");
+    }
+}
